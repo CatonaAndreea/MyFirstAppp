4befe479c51e9a861aca6eec43f3586395339fff
andreea catona <c_alina96@yahoo.com>
2020-10-16 12:49:19 +0300

    Am adaugat MyClass

---
+++ MyFirstAppp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstAppp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
